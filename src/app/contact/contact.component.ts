ebf779a40c40c0d8b06041a2c4d0800bb4c44bb5
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2023-02-28 11:56:25 -0500

    completed form logic.. need email api leverage onSubmit() and css

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,13 +7,16 @@ import { ContactProtoype } from '../contactProto';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-  requestType = ['Custom Order', 'Work Inquiry', 'Information']
+  requestType = ['Custom Order', 'Work Inquiry', 'Product Inquiry', 'Other']
 
-  model = new ContactProtoype('Jane', 'jane@example.com', 'Leave your message here.', 'Custom Order', '555-555-5555');
+  model = new ContactProtoype('Enter Name', 'jane@example.com', 'Leave your message here.', 'Custom Order', '555-555-5555');
 
   submitted = false;
 
-  onSubmit() { this.submitted = true; }
+  onSubmit() {
+    this.submitted = true;
+    //add logic to email via a JSON object? 
+  }
 
   newRequest() {
     this.model = new ContactProtoype('', '', '', '');
