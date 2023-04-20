e1c3f234ff4f78f1937bb7c22acbd7d335bda1f8
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2023-04-20 15:33:56 -0400

    added calls to DELETE request for all files in the FormData sent to POST... names changed when sent to uploads so must change the forEach qualifier?

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -12,11 +12,45 @@ export class ContactService {
 
   constructor(private http: HttpClient) { }
 
-  sendEmail(data: FormData): Observable<any> { //you added FormData to accomodate test 'sendEmail' method written by GPT
-    console.log(data);
+  // sendEmail(data: FormData): Observable<any> { //you added FormData to accomodate test 'sendEmail' method written by GPT
+  //   console.log(data);
+  //   return this.http.post<any>(this.apiUrl, data).pipe(
+  //     switchMap(response => of(response)),
+  //     catchError(error => of(error))
+  //   );
+  // }
+
+  // sendEmail(data: FormData): Observable<any> {
+  //   return this.http.post<any>(this.apiUrl, data).pipe(
+  //     switchMap(response => {
+  //       // Delete the uploaded files here
+  //       const fileNames = data.getAll('file');
+  //       fileNames.forEach(fileName => {
+  //         console.log(fileName);
+  //         return this.http.delete(`http://localhost:3000/uploads/${fileName}`);
+  //       });
+  //       return of(response);
+  //     }),
+  //     catchError(error => of(error))
+  //   );
+  // }
+
+  sendEmail(data: FormData): Observable<any> {
     return this.http.post<any>(this.apiUrl, data).pipe(
-      switchMap(response => of(response)),
+      switchMap(response => {
+        // Delete the uploaded files here
+        const fileNames = data.getAll('files');
+        console.log('File names:', fileNames);
+        fileNames.forEach(fileName => {
+          console.log('Deleting file:', fileName);
+          return this.http.delete(`http://localhost:3000/uploads/${fileName}`);
+        });
+        return of(response);
+      }),
       catchError(error => of(error))
     );
   }
+
+
+
 }
