2c854f99860c4913df287699bbfddde6b4248239
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2023-04-25 14:28:10 -0400

    added new instagram service that calls express app endpoint at api/instagram and updated instagram component to show captions and images

--- src/app/instagram/instagram.component.ts
+++ src/app/instagram/instagram.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InstagramService } from './instagram.service';
 
 @Component({
   selector: 'app-instagram',
   templateUrl: './instagram.component.html',
   styleUrls: ['./instagram.component.scss']
 })
-export class InstagramComponent {
+export class InstagramComponent implements OnInit {
+  images: any[] = [];
 
+  constructor(private instagramService: InstagramService) { }
+
+  ngOnInit() {
+
+    // Call the Instagram service to fetch the images and update the images array
+    this.instagramService.getMedia().subscribe((data: any) => {
+      //this is entire json body parsed per individual post object. Parsed further into the 'media_urls' in the template
+      this.images = data.data;
+    });
+  }
 }
