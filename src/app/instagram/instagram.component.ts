0c83426216e0b598db2d55082935a8960a4fd548
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2023-04-27 15:26:42 -0400

    added notes and prep for building out carousel functionality

--- src/app/instagram/instagram.component.ts
+++ src/app/instagram/instagram.component.ts
@@ -8,6 +8,7 @@ import { InstagramService } from './instagram.service';
 })
 export class InstagramComponent implements OnInit {
   images: any[] = [];
+  carouselArray: any[] = [];
   //just added?
   imageEnlarged: any;
 
@@ -29,6 +30,15 @@ export class InstagramComponent implements OnInit {
       this.imageEnlarged = null;
     } else {
       this.imageEnlarged = image;
+      //if the post is a carousel you need to populate this.carouselArray as below and then build out the instagram service to make invidual id calls
+      //from the express get call you just made. then populte the carouselArray with the reponse media_urls instead... maybe have an array in this function
+      //to simplify and then only push the media_urls? after that you'll need to handle them somehow.
+      //would this be more efficient to do onInit and then you just already have the photos there? seems like it might be a slow click function
+      // if (this.imageEnlarged.media_type === 'CAROUSEL_ALBUM') {
+      //   this.carouselArray = this.imageEnlarged.children.data;
+      //   //data array contains id: value pairs.. use the ids from each (.forEach?) to make api calls to get media_urls from each id
+      //   console.log(this.carouselArray)
+      // }
     }
     console.log('Image enlarged:', this.imageEnlarged);
   }
