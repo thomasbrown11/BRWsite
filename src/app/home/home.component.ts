87e499fb5f7250d3db6221bebdb54da47d9d5155
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2023-06-01 12:46:58 -0400

    added newsletter sub method to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,13 +11,37 @@ export class HomeComponent {
   emailSubmitted: boolean = false; //show thank you after successful submission
   isVerifiedEmail: boolean = true; //show error if email validation fails
   errorCode: any = ''; //returned error from email validation
+  isLoading: boolean = false;
 
   constructor(private contactService: ContactService) { }
 
+  //send email address to business and thank you to user
   submitEmail() {
-    // Implement your logic for submitting the email here
-    console.log(this.emailValue); // Example: Logging the email value to the console
-    this.emailValue = '';
-    this.emailSubmitted = true;
+
+    this.isLoading = true;
+
+    this.contactService.sendNewsletterSub(this.emailValue).subscribe(
+      response => {
+        //if validation fails toggle error message on and displace error code
+        if (response.status === 400) {
+          const responseBody = response.error //get object from error response body
+          this.isVerifiedEmail = false;
+          this.errorCode = responseBody.errorCode;
+          console.log('Error Code:', this.errorCode);
+          console.log(this.isVerifiedEmail);
+          this.isLoading = false;
+          return
+        }
+        console.log('Email sent!', response)
+        this.emailSubmitted = true; //submitted triggers thank you element
+        this.isLoading = false; //disables loader
+        this.isVerifiedEmail = true; //toggle verification error message if applicable
+      },
+      error => {
+        console.log('Error sending email:', error);
+        this.isLoading = false; //disables loader
+      }
+    );
   }
+
 }
