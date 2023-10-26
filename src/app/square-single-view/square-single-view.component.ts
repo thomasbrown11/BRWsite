630c098d44ada4bb3f9a1401ff9f356a60180939
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2023-10-26 10:35:57 -0400

    migrating single item view logic to square single view component

--- src/app/square-single-view/square-single-view.component.ts
+++ src/app/square-single-view/square-single-view.component.ts
@@ -1,10 +1,107 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SquareService } from '../square/square.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-square-single-view',
   templateUrl: './square-single-view.component.html',
   styleUrls: ['./square-single-view.component.scss']
 })
-export class SquareSingleViewComponent {
+export class SquareSingleViewComponent implements OnInit {
+
+  id: string | null = ''; //id pulled from the link route parameter
+  imageMap: any = {}; //imageMap contains urls with keys equal to item id
+  itemEnlarged: any; //controls single item view
+  currentImage: any = {};  //current image shown in enlarged image view
+  currentImageIndex: number = 0; //used to toggle via item_data.image_ids array
+  isBubbleSelected: boolean = false; // handle styling on bubble selected
+
+  colorMap: any = {
+    "Spotted Veridian": "#1A8F72",
+    "Translucent Blue Emerald": "#07ADAD",
+    "Red": "#FF0000",
+    "Blue": "#5174D1",
+    "Yellow": "#F7C03E",
+    "White": "#FFFFFF"
+  }
+
+  colorArray: any[] = []; //populate array with array values
+  colorIsSelected: boolean = false; //toggle image array views to color variants
+  currentColorIndex: number = 0;
+
+  constructor(private route: ActivatedRoute, private squareService: SquareService) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      // Get the 'category' parameter and parse it back to an object
+      const itemId = params.get('id');
+      this.id = itemId;
+    });
+
+    this.fetchCatalogData();
+
+    console.log(this.itemEnlarged);
+  }
+
+
+  fetchCatalogData() {
+    this.squareService.getCatalogue().subscribe(data => {
+      // Find item based on route parameter id string match
+      const matchedItem = data.items.find((item: any) => item.id === this.id);
+
+      if (matchedItem) {
+
+        //populate imageMap
+        this.squareService.getImages().subscribe(data => {
+        this.imageMap = data;
+        });
+        //set itemEnlarged to matched item
+        this.itemEnlarged = matchedItem;
+        this.currentImage = this.imageMap[this.itemEnlarged.item_data.image_ids[0]]; //initiate viewable image in item as first image id in array
+      }
+    });
+  }
+
+  formatPrice(price: number): string {
+    // Divide the price by 100 to convert it to dollars
+    const dollars = price / 100;
+    // Format the price with two decimal places and add the "$" symbol
+    return `$${dollars}`;
+  }
+
+    //added to toggle to single item view
+    toggleImageEnlarged(item: any) {
+      console.log('Toggle image:', item);
+      // console.log('carouselIndex', this.carouselIndex)
+      if (this.itemEnlarged === item) {
+        this.itemEnlarged = null; //close single item view
+        this.currentImage = null; //clear image from carousel
+        this.currentImageIndex = 0; //clear out any toggled index values
+      } else {
+        this.itemEnlarged = item; //update current single item.. triggers ngIf
+        this.currentImage = this.imageMap[this.itemEnlarged.item_data.image_ids[0]]; //initiate viewable image in item as first image id in array
+        console.log(`itemEnlarged toggled... instantiating current image as ${this.currentImage}.. index is ${this.currentImageIndex}`)
+      }
+      console.log('Image enlarged:', this.itemEnlarged);
+    }
+
+
+    //this is fucked need to think through references
+    //logic for next and previous requires referencing the currentImageIndex.. so bubble clicks will needs to update the currentImageIndex to retain function
+   chooseImage(id: string) {
+    this.currentImage = this.imageMap[id];
+    console.log(`fired choseImage.. ${this.currentImage}, ${this.currentImageIndex}`)
+   }
+
+   //handles events when bubble is clicked for image selection in single view
+   selectBubble(index: number): void {
+    this.currentImageIndex = index; // Update the selected index
+    this.isBubbleSelected = true; // Enable the class on the selected bubble
+  }
+
+  selectColor(variantIndex: number): void {
+    this.currentColorIndex = variantIndex;
+    this.colorIsSelected = true;
+  }
 
 }
