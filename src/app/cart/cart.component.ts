1bcff7785e9685f7c5492def4b2ccf4aa7b5824e
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2023-11-07 21:08:45 -0500

    added styling and some conditional formatting for cart items

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CacheService } from '../cache.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
 
+  cart : any[] = [];
+
+  constructor(private cacheService: CacheService) {}
+
+  ngOnInit(): void {
+    // Fetch the cart data when the component initializes
+    this.cart = this.cacheService.getCart();
+  }
+
+  formatPrice(price: number): string {
+    // Divide the price by 100 to convert it to dollars
+    const dollars = price / 100;
+    // Format the price with two decimal places and add the "$" symbol
+    return `$${dollars}`;
+  }
 }
