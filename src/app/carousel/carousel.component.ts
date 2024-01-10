d2b7c405597faf9aa58b412a271ad5964adce974
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2024-01-10 14:07:51 -0500

    edited carousel code but need testing

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 interface CarouselItem {
-  imageURL: string;
-  linkUrl: string;
-  text: string;
+  imageUrl: string;
+  linkUrl: string | null;
+  text: string | null;
 }
 
 @Component({
@@ -12,14 +12,15 @@ interface CarouselItem {
   styleUrls: ['./carousel.component.scss']
 })
 
-export class CarouselComponent {
+export class CarouselComponent implements OnInit {
 
 
   carousel: CarouselItem[] | null = null;
 
-  placeholderImage: any = '../../assets/image-placeholder.png';
-  currentImage: any = {};
-  itemEnlarged: any; //controls single item view
+  placeholderImage: string = '../../assets/image-placeholder.png';
+  currentImage: string | null = null;
+  currentLink : string | null = null;
+  currentDescription: string | null = null;
   currentImageIndex: number = 0; //used to toggle via item_data.image_ids array
   isBubbleSelected: boolean = false; // handle styling on bubble selected
 
@@ -27,6 +28,40 @@ export class CarouselComponent {
 
   constructor () {}
 
+  ngOnInit(): void {
+    //populate carousel here:
+    // this.carousel = [];
+
+    //populate carousel with first item
+    this.chooseCarouselItem(this.currentImageIndex);
+
+  }
+
+  //change carousel item based on carousel index (use currentImageIndex as arg on init)
+  chooseCarouselItem(index: number) {
+    //if carousel
+    if (this.carousel) {
+      //populate image if image present
+      if (this.carousel[index].imageUrl) {
+        this.currentImage = this.carousel[this.currentImageIndex].imageUrl;
+      } else {
+        //is this necessary? Might be better to not populate at all
+        this.currentImage = this.placeholderImage;
+      }
+
+      //populate button link if present
+      if (this.carousel[this.currentImageIndex].linkUrl) {
+        this.currentLink = this.carousel[this.currentImageIndex].linkUrl;
+      }
+
+      //populate description if present
+      if (this.carousel[this.currentImageIndex].text) {
+        this.currentDescription = this.carousel[this.currentImageIndex].text;
+      }
+    }
+  }
+
+  //from previous component
   chooseImage(id: string) {
     this.currentImage = this.imageMap[id];
     console.log(`fired choseImage.. ${this.currentImage}, ${this.currentImageIndex}`)
