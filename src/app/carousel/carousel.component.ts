e269bb0dab9fe53cc593f055ed30b1b7b7cd094e
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2024-01-10 13:27:47 -0500

    copypasted square single view carousel and will adapt

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -13,5 +13,29 @@ interface CarouselItem {
 })
 
 export class CarouselComponent {
+
+
   carousel: CarouselItem[] | null = null;
+
+  placeholderImage: any = '../../assets/image-placeholder.png';
+  currentImage: any = {};
+  itemEnlarged: any; //controls single item view
+  currentImageIndex: number = 0; //used to toggle via item_data.image_ids array
+  isBubbleSelected: boolean = false; // handle styling on bubble selected
+
+  imageMap: any = {}; //imageMap contains urls with keys equal to item id
+
+  constructor () {}
+
+  chooseImage(id: string) {
+    this.currentImage = this.imageMap[id];
+    console.log(`fired choseImage.. ${this.currentImage}, ${this.currentImageIndex}`)
+   }
+
+   //handles events when bubble is clicked for image selection in single view
+   selectBubble(index: number): void {
+    this.currentImageIndex = index; // Update the selected index
+    this.isBubbleSelected = true; // Enable the class on the selected bubble
+  }
+
 }
