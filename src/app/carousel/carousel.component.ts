80913dd96c0af0a5593155bb509bf9ccdf5b94a4
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2024-01-10 12:37:16 -0500

    fixed component to fix compile error

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-carousel',
-  templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
-})
-
 interface CarouselItem {
   imageURL: string;
   linkUrl: string;
   text: string;
 }
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss']
+})
 
 export class CarouselComponent {
   carousel: CarouselItem[] | null = null;
